519e696f2c7e6776b08a15e422a9691c2d709128
“Baburaj” <sbrbabu123@gmail.com>
2024-02-25 17:17:12 +0530

    delete function added

--- film_media_app/urls.py
+++ film_media_app/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import Home,Individual_post,Add_post, Update_post
+from .views import Home,Individual_post,Add_post, Update_post, Delete_post
 
 
 urlpatterns = [
@@ -7,7 +7,6 @@ urlpatterns = [
     path("post/<int:pk>",Individual_post.as_view(), name="individual_post"), # Individual post page url
     path("add_post/",Add_post.as_view(), name="add_post"), # add post page url
     path("update_post/<int:pk>",Update_post.as_view(), name="update_post"), # update post page url
-
-
+    path("delete_post/<int:pk>",Delete_post.as_view(), name="delete_post"), # delete post page url
 
 ]
--- film_media_app/views.py
+++ film_media_app/views.py
@@ -1,25 +1,26 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView, ListView, DetailView, CreateView, UpdateView
+from django.urls import reverse_lazy
+from django.views.generic import TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
 
 
 # home page view
 class Home(ListView):
     model = Post
-    template_name = "film_media_app\home.html"
+    template_name = "film_media_app/home.html"
     context_object_name = "posts"
 
 # View for individual posts
 class Individual_post(DetailView):
     model = Post
-    template_name = "film_media_app\individual_post.html"
+    template_name = "film_media_app/individual_post.html"
     context_object_name = "post"
 
 
 # View for adding post
 class Add_post(CreateView):
     model = Post
-    template_name = "film_media_app\\add_post.html"
+    template_name = "film_media_app/add_post.html"
     context_object_name = "post"
     fields = "__all__"
     
@@ -29,3 +30,10 @@ class Update_post(UpdateView):
     model = Post
     template_name = "film_media_app/update_post.html"
     fields = ("title", "blog")
+
+# View for delete post
+class Delete_post(DeleteView):
+    model = Post
+    template_name = "film_media_app/delete_post.html"
+    success_url = reverse_lazy("home")
+    context_object_name ="post"
